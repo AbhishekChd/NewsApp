272e5ec8a1d3f14f789b5ad682efe9f70f9a6de2
AbhishekChd <acechd97@gmail.com>
2018-10-21 16:33:26 +0530

    Included available countries in Specification

--- app/src/main/java/com/example/abhishek/newsapp/models/Specification.java
+++ app/src/main/java/com/example/abhishek/newsapp/models/Specification.java
@@ -2,9 +2,19 @@ package com.example.abhishek.newsapp.models;
 
 import com.example.abhishek.newsapp.network.NewsApi;
 
+import java.util.Locale;
+
 public class Specification {
+    // List of available countries
+    private static final String[] COUNTRIES_AVAILABLE = {"ae", "ar", "at", "au", "be", "bg", "br",
+            "ca", "ch", "cn", "co", "cu", "cz", "de", "eg", "fr", "gb", "gr", "hk", "hu", "id",
+            "ie", "il", "in", "it", "jp", "kr", "lt", "lv", "ma", "mx", "my", "ng", "nl", "no",
+            "nz", "ph", "pl", "pt", "ro", "rs", "ru", "sa", "se", "sg", "si", "sk", "th", "tr",
+            "tw", "ua", "us", "ve", "za"};
+
     private String category;
-    private String country;
+    // Default country
+    private String country = Locale.getDefault().getCountry().toLowerCase();
 
     public String getCategory() {
         return category;
