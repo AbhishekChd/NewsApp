4c1b7432d453ae891eba7db01cf71473d85e0edc
AbhishekChd <acechd97@gmail.com>
2018-10-21 19:25:56 +0530

    Changed bottom navigation icons and name. Added Action bar with centered title

--- app/src/main/java/com/example/abhishek/newsapp/ui/MainActivity.java
+++ app/src/main/java/com/example/abhishek/newsapp/ui/MainActivity.java
@@ -4,6 +4,7 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
@@ -20,13 +21,13 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    binding.message.setText(R.string.title_home);
+                    binding.message.setText(R.string.title_headlines);
                     return true;
                 case R.id.navigation_dashboard:
-                    binding.message.setText(R.string.title_dashboard);
+                    binding.message.setText(R.string.title_saved);
                     return true;
                 case R.id.navigation_notifications:
-                    binding.message.setText(R.string.title_notifications);
+                    binding.message.setText(R.string.title_sources);
                     return true;
             }
             return false;
@@ -38,6 +39,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        setSupportActionBar(binding.toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(getString(R.string.app_name));
+        }
     }
 
 }
