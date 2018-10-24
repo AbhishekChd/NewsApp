503160783702aee3573beb5e98ce32f8d84f57b5
AbhishekChd <acechd97@gmail.com>
2018-10-24 18:22:55 +0530

    Added share and read more listener to Details Activity

--- app/src/main/java/com/example/abhishek/newsapp/ui/news/DetailActivity.java
+++ app/src/main/java/com/example/abhishek/newsapp/ui/news/DetailActivity.java
@@ -1,8 +1,11 @@
 package com.example.abhishek.newsapp.ui.news;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.example.abhishek.newsapp.R;
 import com.example.abhishek.newsapp.databinding.ActivityDetailBinding;
@@ -18,9 +21,27 @@ public class DetailActivity extends AppCompatActivity {
 
         Bundle bundle = getIntent().getExtras();
         if (bundle != null && bundle.containsKey(PARAM_ARTICLE)) {
-            Article article = bundle.getParcelable(PARAM_ARTICLE);
+            final Article article = bundle.getParcelable(PARAM_ARTICLE);
             if (article != null) {
                 binding.setArticle(article);
+                binding.ivShare.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(Intent.ACTION_SEND);
+                        String shareText = article.getTitle() + "\n" + article.getUrl();
+                        intent.putExtra(Intent.EXTRA_TEXT, shareText);
+                        intent.setType("text/plain");
+
+                        startActivity(intent);
+                    }
+                });
+                binding.btnReadFull.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(article.getUrl()));
+                        startActivity(intent);
+                    }
+                });
             }
         }
     }
