6633b41945780d189584cc4ac54df78c9bdd07d0
AbhishekChd <acechd97@gmail.com>
2018-10-26 19:56:39 +0530

    Added source on click response

--- app/src/main/java/com/example/abhishek/newsapp/ui/sources/SourceFragment.java
+++ app/src/main/java/com/example/abhishek/newsapp/ui/sources/SourceFragment.java
@@ -2,7 +2,9 @@ package com.example.abhishek.newsapp.ui.sources;
 
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -87,7 +89,8 @@ public class SourceFragment extends Fragment implements SourceAdapter.SourceAdap
 
     @Override
     public void onSourceItemClicked(Source source) {
-
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(source.getUrl()));
+        startActivity(intent);
     }
 
     @Override
