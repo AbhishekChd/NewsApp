cd32af75c8c2d61a6556d2e0dda3f37943734a66
Abhishek Chaudhary <acechd97@gmail.com>
2018-11-09 11:12:28 +0530

    Added persistent recycler view state across rotations

--- app/src/main/java/com/example/abhishek/newsapp/ui/news/NewsFragment.java
+++ app/src/main/java/com/example/abhishek/newsapp/ui/news/NewsFragment.java
@@ -5,10 +5,12 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -29,10 +31,12 @@ import timber.log.Timber;
 
 public class NewsFragment extends Fragment implements NewsAdapter.NewsAdapterListener {
     public static final String PARAM_CATEGORY = "param-category";
+    public static final String PARAM_LIST_STATE = "param-state";
     private final NewsAdapter newsAdapter = new NewsAdapter(null, this);
     private NewsApi.Category newsCategory;
     private NewsFragmentBinding binding;
     private boolean showSaved = false;
+    private Parcelable listState;
 
     public static NewsFragment newInstance(NewsApi.Category category) {
         NewsFragment fragment = new NewsFragment();
@@ -75,6 +79,9 @@ public class NewsFragment extends Fragment implements NewsAdapter.NewsAdapterLis
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        if (savedInstanceState != null) {
+            listState = savedInstanceState.getParcelable(PARAM_LIST_STATE);
+        }
         NewsViewModel viewModel = ViewModelProviders.of(this).get(NewsViewModel.class);
         if (showSaved) {
             viewModel.getAllSaved().observeForever(new Observer<List<Article>>() {
@@ -82,8 +89,10 @@ public class NewsFragment extends Fragment implements NewsAdapter.NewsAdapterLis
                 public void onChanged(@Nullable List<Article> articles) {
                     if (articles != null) {
                         newsAdapter.setArticles(articles);
-                    }else{
+                        restoreRecyclerViewState();
+                    } else {
                         newsAdapter.notifyDataSetChanged();
+                        restoreRecyclerViewState();
                     }
                 }
             });
@@ -95,6 +104,7 @@ public class NewsFragment extends Fragment implements NewsAdapter.NewsAdapterLis
                 public void onChanged(@Nullable List<Article> articles) {
                     if (articles != null) {
                         newsAdapter.setArticles(articles);
+                        restoreRecyclerViewState();
                     }
                 }
             });
@@ -123,6 +133,22 @@ public class NewsFragment extends Fragment implements NewsAdapter.NewsAdapterLis
             bottomSheet.show(getActivity().getSupportFragmentManager(), bottomSheet.getTag());
         } else {
             Timber.e("No Parent Activity was found!");
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        if (binding.rvNewsPosts.getLayoutManager() != null) {
+            listState = binding.rvNewsPosts.getLayoutManager().onSaveInstanceState();
+            outState.putParcelable(PARAM_LIST_STATE, listState);
+        }
+    }
+
+    private void restoreRecyclerViewState() {
+        if (binding.rvNewsPosts.getLayoutManager() != null) {
+            binding.rvNewsPosts.getLayoutManager().onRestoreInstanceState(listState);
         }
     }
 }
